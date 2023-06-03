d473220b54268d459d73044361f55ab816b41a2c
ssamarasam <samcsegct@gmail.com>
2023-06-03 18:13:44 +0530

    Cleanum game cards - alignement

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,16 +10,16 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card >
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading size={"md"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading size={"md"}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
